7e7e0e74dc62117dfcdc87fee4b6c21e5a44fc3f
accio-rajitha <159473339+accio-rajitha@users.noreply.github.com>
2024-04-17 18:04:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let audio = null;
+
+function playSound(soundFile) {
+  // Stop any currently playing sound
+  stopSound();
+
+  // Create new audio element
+  audio = new Audio(`sounds/${soundFile}`);
+  
+  // Play the audio
+  audio.play();
+}
+
+function stopSound() {
+  // If audio is currently playing, stop it
+  if (audio !== null) {
+    audio.pause();
+    audio.currentTime = 0;
+  }
+}
