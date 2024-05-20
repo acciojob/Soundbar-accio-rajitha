dc676496216929cf2504a31c594e42a6fd039435
accio-rajitha <159473339+accio-rajitha@users.noreply.github.com>
2024-05-20 14:38:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //your JS code here. If required.
-let audio = new Audio();
+let audio = document.getElementById('audio');
 
 function playSound(soundFile) {
-audio.src = soundFile;
-audio.play();
+    audio.src = soundFile;
+    audio.play();
 }
 
 function stopSound() {
-audio.pause();
-audio.currentTime = 0;
-} 
+    audio.pause();
+    audio.currentTime = 0;
+}
